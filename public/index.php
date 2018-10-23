72bc7c35cec77d4be7ff577e0c5b3b1add4bfc77
3A417012 <nds10745@gmail.com>
2018-10-23 13:33:58 +0800

    adding mary & john in ./public/index.php

--- public/index.php
+++ public/index.php
@@ -10,4 +10,8 @@ $world = new World();
 $lara = new Lara();
 $vincent = new Hello\Someone('Vincent');
 
+// 以下Someone類別的使用可以不用use Demo\Hello; 
+$mary= new \Demo\Hello\Someone('Mary');
+$john= new Demo\Hello\Someone('John');
+
 ?>
